ba24bf5cc2ec32f90e7a11c39645cff6ef350bc5
Jacob Namhyung Kim <87889917+everywherejacobkim@users.noreply.github.com>
2023-09-01 20:26:46 -0700

    Chore: install prisma & create schema for models

--- src/app/components/sidebar/Followbar.tsx
+++ src/app/components/sidebar/Followbar.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const Followbar = () => {
   return (
     <div className="w-full mt-10 text-white">
-      <div className="bg-primary-50 p-4 mx-4 rounded-lg">
+      <div className="bg-primary-30/30 p-4 mx-4 rounded-lg">
         <h1 className="">People you may know</h1>
       </div>
     </div>

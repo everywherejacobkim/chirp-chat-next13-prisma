1da3e9b04306c9c8a358e267b710e5f03b1ce1fb
Jacob Namhyung Kim <87889917+everywherejacobkim@users.noreply.github.com>
2023-09-04 20:32:03 -0700

    Fix: fix object handling error

--- src/app/components/header/Header.tsx
+++ src/app/components/header/Header.tsx
@@ -9,7 +9,7 @@ const Header = ({ label }: { label: string }) => {
     <div className="border-b-[1px] border-neutral-800 p-5">
       <div className="flex flex-col gap-2">
         <h1 className="text-primary-text text-xl font-semibold">{label}</h1>
-        <h3 className="text-primary-text">Welcome back {data.name}</h3>
+        <h3 className="text-primary-text">Welcome back {""}</h3>
       </div>
     </div>
   );

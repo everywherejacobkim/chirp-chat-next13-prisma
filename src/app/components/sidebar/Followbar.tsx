229064bb4b77fe6540ba0e1fd6e15c5dad4dac5e
Jacob Namhyung Kim <87889917+everywherejacobkim@users.noreply.github.com>
2023-09-09 00:46:48 -0700

    Feat: create avatar component and fetch user data to followbar

--- src/app/components/sidebar/Followbar.tsx
+++ src/app/components/sidebar/Followbar.tsx
@@ -1,10 +1,30 @@
+"use client";
 import React from "react";
+import useUsers from "@/libs/hooks/useUsers";
+import Avatar from "@/components/avatar/Avatar";
 
 const Followbar = () => {
+  const { data: users = [] } = useUsers();
+
+  if (users.length === 0) {
+    return null;
+  }
+
   return (
     <div className="w-full mt-10 text-white">
       <div className="bg-primary-30/30 p-4 mx-4 rounded-lg">
         <h1 className="">People you may know</h1>
+        <div className="flex flex-col gap-4 mt-2">
+          {users.map((user: Record<string, any>) => (
+            <div key={user.id} className="flex flex-row gap-4">
+              <Avatar userId={user.id} />
+              <div className="flex flex-col">
+                <p className="text-white font-semibold text-sm">{user.name}</p>
+                <p className="text-neutral-400 text-sm">@{user.username}</p>
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
